d9ee5ad2b806feaf94ceb46f5e6645f184754b59
YuWonJong <wonbell00@gmail.com>
2025-06-04 22:21:45 +0900

    sort

---
+++ src/dataStructures_algorithms/sort/BubbleSort.java
@@ -0,0 +1,27 @@
+package dataStructures_algorithms.sort;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+            BubbleSort m=new BubbleSort();
+            int [] array={5,3,8,1,2,7};
+            m.bubbleSortinf(array);
+            
+    }
+
+    public void bubbleSortinf(int [] arr){
+        for(int i=0; i<arr.length-1; i++)
+        {
+            for(int j=0; j<arr.length-1-i; j++){
+
+               if(arr[j]>arr[j+1]){
+                int change=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=change;
+               } 
+            }
+        }
+        for(int value:arr){
+            System.out.println(value);
+        }
+    }
+}
---
+++ src/dataStructures_algorithms/sort/InsertionSort.java
@@ -0,0 +1,40 @@
+package dataStructures_algorithms.sort;
+
+public class InsertionSort {
+    public static void insertionSort(int[] list) {
+        int n = list.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = list[i];  // 현재 삽입할 값
+            int j = i - 1;
+
+            // 앞쪽 정렬된 부분에서 key보다 큰 값은 오른쪽으로 밀기
+            while (j >= 0 && list[j] > key) {
+                list[j + 1] = list[j];
+                j--;
+            }
+
+            // key를 올바른 위치에 삽입
+            list[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 3, 5, 2, 4};
+
+        System.out.println("정렬 전:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("정렬 후:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
